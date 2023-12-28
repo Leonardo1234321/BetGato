9bdb45fd200b23f511c5e2fb752556e1fcb719c0
Leonardo1234321 <153666018+Leonardo1234321@users.noreply.github.com>
2023-12-28 19:25:06 -0300

    Update PROJECT_BETCAVALO.py

--- PROJECT_BETCAVALO.py
+++ PROJECT_BETCAVALO.py
@@ -3,6 +3,9 @@ from sys import exit
 from random import randint
 pygame.init()
 screen = pygame.display.set_mode((1280, 660))
+tempo_inicial = 60000
+
+pygame.mixer.music.load("musica_fundo.mpeg")
 
 class gato:
     def __init__(self):
@@ -11,12 +14,12 @@ class gato:
         self.index_float = float(0)
         self.speed = 0
         self.valor_apostado = 0
-        self.keys = pygame.K_d
-    def spritecheetos(self, x, y):
+        self.keys = pygame.K_0
+    def spritecheetos(self, x, y, a , b):
         for i in range(0, x):
             self.sprites.append(pygame.image.load(f"gato{y}_correndo/tile00{i}.png"))
         for i in range(0, x):
-            self.sprites[i] = pygame.transform.scale(self.sprites[i], (200, 200))
+            self.sprites[i] = pygame.transform.scale(self.sprites[i], (a, b))
     def Gatorect(self, x, y):
         self.sprite_rect = self.sprites[self.index_int].get_rect(center=(x, y))
     def gato_animacao(self, v, s):
@@ -37,12 +40,17 @@ class gato:
         elif self.sprite_rect.left <= 0: 
             self.sprite_rect.left = 0
     def corrida(self):
-        self.speed -= 0.1
+        if self.sprite_rect.right < (screen.get_width() / 3):
+            self.speed -= 0.1
+        elif (screen.get_width() / 3) <= self.sprite_rect.right <= ((2*screen.get_width()) / 3):
+            self.speed -= 0.2
+        elif ((2*screen.get_width()) / 3) <= self.sprite_rect.right:
+            self.speed -= 0.3
         if self.speed >= 0:
             self.gato_animacao(0.5 + (self.speed/10), 2)
         else:
             self.gato_animacao(0.5, 2)
-        self.gato_movimento(5, -3)
+        self.gato_movimento(5, -5)
   
 fundo1 = [pygame.transform.scale(pygame.image.load("fundo01.jpeg"), (1280, 660)), 
           pygame.transform.scale(pygame.image.load("fundo01.jpeg"), (1280, 660))]
@@ -59,42 +67,70 @@ def fundo_dinamico(x):
         
 
 gato01 = gato()
-gato01.spritecheetos(8, 1)
+gato01.spritecheetos(8, 1, 200, 200)
 gato01.Gatorect(100, 500)
+gato01.keys = pygame.K_w
 
 gato02 = gato()
-gato02.spritecheetos(8, 2)
+gato02.spritecheetos(8, 2, 200, 200)
 gato02.Gatorect(100, 550)
+gato02.keys = pygame.K_d
 
 gato03 = gato()
-gato03.spritecheetos(8, 3)
+gato03.spritecheetos(8, 3, 200, 200)
 gato03.Gatorect(100, 600)
+gato03.keys = pygame.K_a
 
 clock = pygame.time.Clock()
 
+text_Font = pygame.font.Font("minecraft_font.ttf", 40)
+
+jogo = False
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == gato01.keys:
-                gato01.speed += 0.6
-            if event.key == pygame.K_a:
-                gato02.speed += 0.6
-            if event.key == pygame.K_0:
-                gato03.speed += 0.6
+            if event.type == pygame.QUIT:
+                exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_1:
+                    jogo = True
+    while jogo:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == gato01.keys:
+                    gato01.speed += 1
+                if event.key == gato02.keys:
+                    gato02.speed += 1
+                if event.key == gato03.keys:
+                    gato03.speed += 1
+                if event.key == pygame.K_0:
+                    tempo_inicial = pygame.time.get_ticks() + 60000
+                    pygame.mixer.music.play(123123)
 
-    screen.blit(fundo1[0], fundo_rect[0])
-    screen.blit(fundo1[1], fundo_rect[1])
-    fundo_dinamico(10)
+        tempo = (tempo_inicial - pygame.time.get_ticks()) / 1000
+        if tempo <= 0:
+            break
 
-    screen.blit(gato01.sprites[gato01.index_int], gato01.sprite_rect)
-    screen.blit(gato02.sprites[gato02.index_int], gato02.sprite_rect)
-    screen.blit(gato03.sprites[gato03.index_int], gato03.sprite_rect)
+        texto1 = text_Font.render(f"{tempo:.2f}", False, "white")
+        texto1_rect = texto1.get_rect(center=(640, 50))
 
-    gato01.corrida()
-    gato02.corrida()
-    gato03.corrida()
 
+        screen.blit(fundo1[0], fundo_rect[0])
+        screen.blit(fundo1[1], fundo_rect[1])
+        screen.blit(texto1, texto1_rect)
+        fundo_dinamico(15)
+        screen.blit(gato01.sprites[gato01.index_int], gato01.sprite_rect)
+        screen.blit(gato02.sprites[gato02.index_int], gato02.sprite_rect)
+        screen.blit(gato03.sprites[gato03.index_int], gato03.sprite_rect)
+
+        gato01.corrida()
+        gato02.corrida()
+        gato03.corrida()
+        pygame.time.wait(3)
+        pygame.display.update()
+        clock.tick(30)
+    
+    pygame.time.wait(3)
     pygame.display.update()
     clock.tick(30)
